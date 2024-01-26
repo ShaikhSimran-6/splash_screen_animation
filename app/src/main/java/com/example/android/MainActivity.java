d48a9c09a70f5afb925699b76358a23d5c795732
shaikh simran <shaikhnousheen66@gmail.com>
2024-01-26 19:31:54 +0530

    First Commit

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -4,66 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.os.Handler;
+
 
 public class MainActivity extends AppCompatActivity{
-
-    Button btn;
-    TextView text_1;
-    TextView text_2;
-    ImageView imageView;
-
-    Animation animate_btn;
-    Animation animate_text;
-
-    Animation animate_img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.get_started_btn);
-        text_1 = findViewById(R.id.text_1);
-        text_2 = findViewById(R.id.text_2);
-        imageView = findViewById(R.id.image);
-
-        btn.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable()
+        {
             @Override
-            public void onClick(View view) {
+            public void run()
+            {
                 Intent intent = new Intent(MainActivity.this, SignInSignUpActivity.class);
                 startActivity(intent);
             }
-        });
-
-
-//        Add the animation.
-
-        animate_btn = AnimationUtils.loadAnimation(this, R.anim.animate_btn);
-        btn.setAnimation(animate_btn);
-
-        animate_text = AnimationUtils.loadAnimation(this, R.anim.animate_btn);
-        btn.setAnimation(animate_text);
-
-        animate_img = AnimationUtils.loadAnimation(this, R.anim.animate_img);
-        btn.setAnimation(animate_img);
-
-        btn.setAnimation(animate_btn);
-
-//      Animation for the text
-
-        text_1.setAnimation(animate_text);
-        text_2.setAnimation(animate_text);
-
-        imageView.setAnimation(animate_img);
+        }, 2500);
 
     }
-
-
 }
--- app/src/main/java/com/example/android/SignInSignUpActivity.java
+++ app/src/main/java/com/example/android/SignInSignUpActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
 public class SignInSignUpActivity extends AppCompatActivity {
 
     Button signIn;
